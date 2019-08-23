980339826b6ff5256d2d08a5629224440c86eb4d
FeedFish34 <1073452867@qq.com>
2019-08-23 13:14:17 +0800

    增加checksum

--- OCRForm.py
+++ OCRForm.py
@@ -3,6 +3,7 @@ import pytesseract
 from PIL import Image
 import re
 import logging
+import os
 
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"    # 日志格式化输出
 DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"                        # 日期格式
@@ -11,6 +12,7 @@ fs = logging.StreamHandler()
 logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT, datefmt=DATE_FORMAT, handlers=[fp, fs])
 text = ""
 List = []
+ResMsg = ""
 
 # #调用笔记本内置摄像头，所以参数为0，如果有其他的摄像头可以调整参数为1，2
 # cap=cv2.VideoCapture(0)
@@ -42,6 +44,30 @@ text = pytesseract.image_to_string(Image.open('D:/image2.jpg'), lang='chi_sim')
 text = re.sub("\D", "", text)
 print(text)
 
+
+def do_checksum(source_string):
+     """  Verify the packet integritity """
+     sum = 0
+     max_count = 3
+     count = 0
+     while count < max_count:
+         val = ord(source_string[count + 1]) * 256 + ord(source_string[count])
+         sum = sum + val
+         sum = sum & 0xffffffff
+         count = count + 2
+     if max_count < len(source_string):
+         sum = sum + ord(source_string[len(source_string) - 1])
+         sum = sum & 0xffffffff
+
+     sum = (sum >> 16) + (sum & 0xffff)
+     sum = sum + (sum >> 16)
+     answer = ~sum
+     answer = answer & 0xffff
+     answer = answer >> 8 | (answer << 8 & 0xff00)
+     print(answer)
+     return answer
+
+
 def recv(serial):
     while True:
         data = serial.read_all()
@@ -66,7 +92,9 @@ if __name__ == '__main__':
         EotString = chr(int("04"))
         if (text != ""):
             List.append(EnqString)
-            List.append(StxString + text + EtxString)
+            ResMsg = StxString + text + EtxString
+            logging.debug("校验数据：" + ResMsg)
+            List.append(ResMsg + str(do_checksum(ResMsg)))
             List.append(EotString)
             logging.debug("发送：" + List[0])
             serial.write(bytes(List[0].encode('utf-8')))  # 数据写回
@@ -80,8 +108,9 @@ if __name__ == '__main__':
                     logging.debug("发送2：" + List[0])
                     serial.write(bytes(List[0].encode('utf-8')))  # 数据写回
                     List.pop(0)
-                elif (s == 5 or s == 2 or s == 3):
-                    logging.debug("发送3：" + chr(int("06")))
+                if (s == 5 or s == 2 or s == 3):
+                    print("回复ACK")
+                    logging.debug("发送ACK：" + chr(int("06")))
                     serial.write(bytearray(chr(int("06")), encoding="utf-8"))  # 数据写回
             logging.debug("接收：" + data.decode('utf-8'))
 
